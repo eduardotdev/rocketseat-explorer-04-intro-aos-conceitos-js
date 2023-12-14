77a57d259428338a151af866891b1ba5734cdbe6
Eduardo Tavares <eduardotdev.21@gmail.com>
2023-12-13 23:10:47 -0300

    string

---
+++ 02-tipos-de-dados/01-string.js
@@ -0,0 +1,14 @@
+/*
+  String
+
+  * Cadeia de caracteres ABC
+
+  "" // aspas duplas
+  '' // aspas simples
+  `` // template literals ou template strings
+
+*/
+
+console.log("Eduardo")
+console.log('Eduardo')
+console.log(`Eduardo ${1 + 1}`)
