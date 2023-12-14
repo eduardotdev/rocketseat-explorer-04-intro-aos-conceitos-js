4540d7d8aafcfb0dcd8b5685883056fbbdb35b32
Eduardo Tavares <eduardotdev.21@gmail.com>
2023-12-13 23:46:44 -0300

    conhecendo as variaveis

---
+++ 03-variaveis/01-conhecendo.js
@@ -0,0 +1,11 @@
+// var -> pode ter seu valor alterado
+
+var clima = "Quente"
+
+// let -> pode ter seu valor alterado
+
+let nome = "Eduardo"
+
+// const -> não pode ter seu valor alterado
+
+const idade = 20
---
+++ 03-variaveis/variaveis.md
@@ -0,0 +1,10 @@
+# Variáveis
+
+* Nomes simbólicos para receber agum valor
+* Atalhos de código
+* Identificadores
+* 3 palavras reservadas para criar uma variável:
+  
+  * var
+  * let
+  * const
