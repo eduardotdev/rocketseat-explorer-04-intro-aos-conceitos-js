1adc2a5f30e42ec6c54a7fc256601851b8d1b63c
Eduardo Tavares <eduardotdev.21@gmail.com>
2023-12-13 23:19:57 -0300

    number

---
+++ 02-tipos-de-dados/02-number.js
@@ -0,0 +1,18 @@
+/*
+  Number
+  
+  * Números
+
+  33       // inteiros
+  12.5     // reais - float
+  NaN      // Not a Number
+  Infinity // infinito
+*/
+
+console.log(33)
+
+console.log(12.5)
+
+console.log(25 * "casa")
+
+console.log(42 / 0)
